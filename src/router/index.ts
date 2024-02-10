1b4847820342bfd0404e52cd2437490a55c58348
Franco Martin <main.francomartin@gmail.com>
2024-02-10 05:03:06 -0300

    Add -- new rooute and category meal view

--- src/router/index.ts
+++ src/router/index.ts
@@ -45,6 +45,14 @@ const router = createRouter({
       component: () => import('../views/Area/AreaView.vue')
     },
     {
+      path: '/category/c=:categoryMeal',
+      name: 'categoryMeal',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/CategoryMeal/CategoryMealView.vue')
+    },
+    {
       path: '/test/s=:idMeal',
       name: 'test',
       // route level code-splitting
---
+++ src/views/CategoryMeal/CategoryMealView.vue
@@ -0,0 +1,17 @@
+
+<template>
+    <LayoutContainer>
+        <template #content>
+            <div v-motion-fade class="flex flex-wrap md:gap-4 justify-center flex-1 my-8 md:my-2 h-full">
+                <h2> Category Meal
+                </h2>
+            </div>
+        </template>
+    </LayoutContainer>
+</template>
+
+<script setup lang="ts">
+import LayoutContainer from '@/layout/LayoutContainer.vue';
+
+
+</script>
